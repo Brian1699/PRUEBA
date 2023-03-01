4f22313569b957953d7b5d181d91ec3a2acca9be
Brian <ashe1990@hotmail.com>
2023-02-28 23:15:21 -0600

    serg2

---
+++ routes/desktop/desktop.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/getUpdates", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+});
+
+
+module.exports = router;
